6759f7dba204283491ac42e3941f600b6bada194
chrissaher <chrissaher@gmail.com>
2017-08-10 01:26:15 +0900

    Error en dimensiones de theta Line.69

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -1,34 +1,37 @@
 import numpy as np
+import random
 
 class NeuralNetwork:
-	def __init__(self, inputs, outputs, nTrainingExamples, stepSize =  0.1, nLayers = 1, regularizationParameter = 10):
+	def __init__(self, inputs, outputs, nTrainingExamples, stepSize =  0.1, nLayers = 3, regularizationParameter = 10):
 		self.Xs = inputs
 		self.Ys = outputs
 		self.m = nTrainingExamples
 		self.n = stepSize
 		self.l = nLayers
 		self.Lambda = regularizationParameter
-		self.t = size(self.Xs[0]) + 1
+		self.t = len(self.Xs[0]) + 1
+		print("M: %d T: %d Y:%d"%(self.m, self.t, len(self.Ys[0])))
+		self.theta = np.ones(self.t * self.t * self.l).reshape(self.l ,self.t, self.t)
 
-	def sigmoid(x):
+	def sigmoid(self, x):
 		return 1 / (1 + np.exp(-x))
 
-	def SGD():
-		r = random.sample(xrange(1, m),m)
-		theta = np.ones(self.t * self.t * self.l).reshape(self.l *self.t, self.t)
+	def SGD(self):
+		r = random.sample(range(self.m), self.m)
 		D = np.ones(self.l * self.t).reshape(self.l , self.t)
 		delta = np.zeros(self.l * self.t * self.t).reshape(self.l, self.t, self.t) # Triangulo
-		for i in range(m):
-			X = np.append(1, Xs[r[i]])
-			Y = np.array(Ys[r[i]])
-			Hx = np.ones(len(Y))
+		for i in range(self.m):
+			X = np.append(1, self.Xs[r[i]])
+			Y = np.array(self.Ys[r[i]])
+			Hx = np.ones(np.size(Y))
 			A = np.array([X, [[1 for _t in range(self.t)] for _l in range(self.l - 2)] ,Hx])
-			A = forwardPropagation(A, theta)
+			A = self.forwardPropagation(A)
+
 			Error = np.zeros(self.l * self.t).reshape(self.l, self.t)
-			for _y in range(len(Y)):
+			for _y in range(np.size(Y)):
 				_yLayer = self.l - 1
 				Error[_yLayer][_y] = A[_yLayer][_y] - Y[_y]
-			Error = backPropagation(Error, A, theta)
+			Error = self.backPropagation(Error, A)
 			for _l in range(self.l - 2, 0, -1):
 				for _to in range(self.t):
 					for _from in range(self.t): # LAYER POSTERIOR (L + 1)
@@ -38,33 +41,64 @@ class NeuralNetwork:
 					for _from in range(self.t): # LAYER ANTERIOR (L - 1)
 						D[_l][_to][_from] = (1 / self.m) * delta[_l][_to][_from];
 						if _to != 0 :
-							D[_l][_to][_from] += self.Lambda * theta[_l][_to][_from]
+							D[_l][_to][_from] += self.Lambda * self.theta[_l][_to][_from]
 
 			for _l in range(1, self.l):
 				for _to in range(self.t):
 					for _from in range(self.t):
-						theta[_l][_to][_from] = theta[_l][_to][_from] - n * D[_l][_to][_from]
+						self.theta[_l][_to][_from] = self.theta[_l][_to][_from] - n * D[_l][_to][_from]
 
-	def forwardPropagation(A, theta):
+	def forwardPropagation(self, Activation):
 		for _layer in range(1, self.l):
-			nodesInLayer = len(Activation[_layer])
-			for _to in range(1, nodesInLayer):
-				actvivation = 0
-				nodesInBackLayer = len(Activation[_layer -1])
+			nodesInLayer = self.t
+			rangeStart = 1
+			if _layer == self.l - 1:
+				rangeStart = 0
+				nodesInLayer = 10
+			for _to in range(rangeStart, nodesInLayer):
+				nodesInBackLayer = np.size(Activation[_layer -1])
+				act = 0
 				for _from in range(0, nodesInBackLayer): # LAYER ANTERIOR (L - 1)
-					z = theta[_layer][_to][_from] * Activation[_layer - 1][_from]
-					activation += sigmoid(z)
-				Activation[_layer][_to] = activation
+					z = self.theta[_layer][_to][_from] * Activation[_layer - 1][_from]
+					act += self.sigmoid(z)
+				print("_LAYER: %d, _TO: %d" %(_layer, _to))
+				print(np.shape(Activation))
+				print(np.shape(Activation[_layer]))
+				print(np.shape(Activation[_layer][0]))
+				print(np.shape(Activation[_layer][0][0]))
+				Activation[_layer][_to] = act
 		return Activation
 
-	def backPropagation(Error, Activation, theta):
-		for _layer in rang(self.l - 2, 0, -1):
-			nodesInLayer = len(Error[_layer])
+	def backPropagation(self, Error, Activation):
+		for _layer in range(self.l - 2, 0, -1):
+			nodesInLayer = np.size(Error[_layer])
 			for _to in range(1, nodesInLayer):
 				error = 0
-				nodesInFrontLayer = len(Error[_layer + 1])
+				nodesInFrontLayer = np.size(Error[_layer + 1])
 				G = Activation[_layer][_to] * (1 - Activation[_layer][_to])
 				for _from in range(1, nodesInFrontLayer): # LAYER POSTERIOR ( L + 1)
-					error += theta[_layer][_from][_to] * Error[_layer + 1][_from] * G # (*G) Tambien puede ir fuera del for
+					error += self.theta[_layer][_from][_to] * Error[_layer + 1][_from] * G # (*G) Tambien puede ir fuera del for
 				Error[_layer][_to] = error
 		return Error
+
+	def Predict(self, X):
+		Activation = np.array([X, [[1 for _t in range(self.t)] for _l in range(self.l - 2)], [0 for _t in range(10)]])
+		for _layer in range(1, self.l):
+			nodesInLayer = np.size(Activation[_layer])
+			for _to in range(1, nodesInLayer):
+				actvivation = 0
+				nodesInBackLayer = np.size(Activation[_layer -1])
+				for _from in range(0, nodesInBackLayer): # LAYER ANTERIOR (L - 1)
+					z = self.theta[_layer][_to][_from] * Activation[_layer - 1][_from]
+					activation += sigmoid(z)
+				Activation[_layer][_to] = activation
+		print("PREDICTIONS:")
+		for i in range(10):
+			print("I: %d Prediction: %.03lf" %(i, Activation[self.l - 1][i]))
+
+	def printTheta(self):
+		for _l in range(self.l):
+			for i in range(10):
+				for j in range(10):
+					print("theta[%d][%d][%d] =" %(_l, i, j))
+					print(np.shape(self.theta))
--- src/loadData.py
+++ src/loadData.py
@@ -9,32 +9,32 @@ from NeuralNetwork import NeuralNetwork
 def nextInt(file):
 	return int.from_bytes(file.read(4), byteorder = 'big')
 def loadImageFile(file):
-    file.seek(0)
-    magic_number_images = nextInt(file)
-    nImages = nextInt(file)
-    nRow = nextInt(file)
-    nCol = nextInt(file)
-    print("Total Images: ", nImages)
-    print("Size of image: %d x %d" % (nRow, nCol))
-    print("Reading data from training...")
-    X = [[[0 for x in range(nRow)] for y in range(nCol)] for z in range(nImages)]
+	file.seek(0)
+	magic_number_images = nextInt(file)
+	nImages = nextInt(file)
+	nRow = nextInt(file)
+	nCol = nextInt(file)
+	print("Total Images: ", nImages)
+	print("Size of image: %d x %d" % (nRow, nCol))
+	print("Reading data from training...")
+	X = [[0 for x in range(nRow * nCol)] for y in range(nImages)]
 
-    for t in range(0, nImages):
-    	for i in range(0, nRow ):
-    		for j in range(0, nCol):
-    			pixel = file.read(1)
-    			X[t][i][j] = ord(pixel)
-    return X, nImages
+	for j in range(nImages):
+		for i in range(nRow * nCol):
+			pixel = file.read(1)
+			X[j][i] = ord(pixel)
+
+	return X, nImages
 
 def loadLabelFile(file):
-    file.seek(0)
-    print("Reading data from label...")
-    maginc_number_train = nextInt(file)
-    nLabels = nextInt(file)
-    y = [0 for l in range(nLabels)]
-    for l in range(nLabels):
-        y[l] = ord(file.read(1))
-    return y, nLabels
+		file.seek(0)
+		print("Reading data from label...")
+		maginc_number_train = nextInt(file)
+		nLabels = nextInt(file)
+		y = [[0 for j in range(10)] for l in range(nLabels)]
+		for l in range(nLabels):
+				y[l][ord(file.read(1))] = 1
+		return y, nLabels
 
 
 print("Loading training data")
@@ -53,12 +53,22 @@ y, nLabels = loadLabelFile(f)
 readingLabelFile = time.time()
 f.close()
 
+NN = NeuralNetwork(X, y, nLabels)
+print("Training Neural NeuralNetwork")
+NN.SGD()
+trainingNeuralNetwork = time.time()
+print("Testing")
+#NN.Predict(X[0])
+NN.printTheta()
+testingNeuralNetwork = time.time()
 totalTime = time.time()
 print("---")
 print("Loading data time: %.4fsec" % (loadingImageFile - startImageFile))
 print("Reding data time: %.4fsec" % (readingImageFile - loadingImageFile))
 print("Loading label time: %.4fsec" % (loadingLabelFile - startLabelFile))
 print("Reding label time: %.4fsec" % (readingLabelFile - loadingLabelFile))
+print("Training neural network time: %.4fsec" % (trainingNeuralNetwork - readingLabelFile))
+print("Testing neural network time: %.4fsec" % (testingNeuralNetwork - trainingNeuralNetwork))
 print("Total time: %.4fsec" % (totalTime - startImageFile))
 print("Total images process: ", nImages)
 print("Total labels process: ", nLabels)
